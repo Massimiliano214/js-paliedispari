2d219884f1f4a346b6bddb51302ec1193d739b62
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-02 15:11:00 +0100

    impostazione pari o dispari

---
+++ pari e dispari/js/main.js
@@ -0,0 +1,12 @@
+const scelta = prompt("inserire: pari o dispari;");
+const numeroScelto = parseInt(prompt("Inserisci nuero da 1 a 5;")); 
+
+console.log(scelta);
+console.log(numeroScelto);
+
+
+
+
+function numeroCasuale () {
+
+}
