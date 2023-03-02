237bf3a544d99ca55d29aca296635441dcd1cdeb
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-02 15:49:17 +0100

    creazione risultato programma

--- pari e dispari/js/main.js
+++ pari e dispari/js/main.js
@@ -1,12 +1,44 @@
 const scelta = prompt("inserire: pari o dispari;");
 const numeroScelto = parseInt(prompt("Inserisci nuero da 1 a 5;")); 
 
-console.log(scelta);
-console.log(numeroScelto);
+console.log("scelta pari/dispari " + scelta);
+console.log("scelta numero player " + numeroScelto);
 
+const numeroDaMoltiplicare = 5;
+const numeroDaSommare = 1;
 
+const variabileCorta = numeroCasuale (numeroDaMoltiplicare, numeroDaSommare);
 
+console.log("numero pc " + variabileCorta);
 
-function numeroCasuale () {
+const sommaNumeri = numeroScelto + variabileCorta;
 
-}
+console.log("somma dei numeri " + sommaNumeri)
+
+tipoSomma(sommaNumeri);
+
+if ((scelta == "pari") && (risultatoPari)) {
+    console.log("Hai vinto");
+} else if ((scelta == "dispari") && (risultatoPari == false)) {
+    console.log("Hai vinto");
+} else {
+    console.log("Hai perso");
+};
+
+function numeroCasuale (fattore, valoreSommato) {
+    const numeroUscito = Math.floor(Math.random() * fattore) + valoreSommato;
+    console.log("numero casuale " + numeroUscito);
+    return numeroUscito;
+};
+
+function tipoSomma (sommaDaVerificare) {
+    const sommaTotale = sommaDaVerificare;
+    if (sommaTotale % 2 == 0) {
+        risultatoPari = true;
+    } else {
+        risultatoPari = false;
+    };
+
+    console.log(risultatoPari);
+    return risultatoPari;
+};
